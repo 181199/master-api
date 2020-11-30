06e7d34bd01c9147ba3c688f7b58dda652fa6bba
181199 <anjafsalen@gmail.com>
2020-11-30 21:23:38 +0100

    Simple StackOverflow API client

--- src/main/java/StackOverflow_API.java
+++ src/main/java/StackOverflow_API.java
@@ -10,15 +10,16 @@ import org.json.JSONObject;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
+import java.util.Date;
 
 public class StackOverflow_API {
 
     public static void main(String[] args) throws UnsupportedEncodingException {
 
-        String tags = "java";
+        String tags = "java;security";
 
         HttpClient client = HttpClientBuilder.create().build();
-        HttpGet request = new HttpGet("https://api.stackexchange.com/2.2/questions?order=desc&sort=month&tagged=" + URLEncoder.encode(tags, "UTF-8") + "&site=stackoverflow&filter=!--1nZwT3Ejsm");
+        HttpGet request = new HttpGet("https://api.stackexchange.com/2.2/questions?order=desc&sort=activity&tagged=" + URLEncoder.encode(tags, "UTF-8") + "&site=stackoverflow&filter=!--1nZwT3Ejsm");
 
         try {
             HttpResponse response = client.execute(request);
@@ -35,17 +36,21 @@ public class StackOverflow_API {
                 JSONObject oj = tokenList.getJSONObject(i);
                 String title = oj.getString("title");
                 int id = oj.getInt("question_id");
+                int date = oj.getInt("creation_date");
+                Date time = new Date((long)date*1000);
+
                 String body = oj.getString("body");
 
-                System.out.println("Title: " + title + "\nId: " + id + "\nBody: " + body + "\n");
+                System.out.println("Title: " + title + "\nId: " + id + "\nDate: " + time + "\nBody: " + body + "\n");
 
                 int answerNumber = oj.getInt("answer_count");
                 if(answerNumber != 0) {
                     JSONArray answers = oj.getJSONArray("answers");
 
-                    for(int j = 0; j < answers.length(); j++){
-                        String answer = oj.getString("body");
-                        System.out.println("Answer: " + answer);
+                    for(int j = 0; j < answerNumber; j++){
+                        JSONObject answerObj = answers.getJSONObject(j);
+                        String answer = answerObj.getString("body");
+                        System.out.println("Answer " + (j+1) + ": " + answer);
                     }
                 }
             }
