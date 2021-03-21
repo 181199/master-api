046041767601c5fab0a5bbfb2c955f10265ce0e7
181199 <anjafsalen@gmail.com>
2021-03-21 16:33:49 +0100

    Updated methods

--- src/main/java/machinelearning/utils/MergeFiles.java
+++ src/main/java/machinelearning/utils/MergeFiles.java
@@ -10,13 +10,23 @@ import java.util.*;
 public class MergeFiles {
 
     public static void main(String[] args) throws IOException {
-        List<Path> paths = Arrays.asList(Paths.get("/Users/anja/Desktop/master/api/files/testing/stackoverflowSR_new.csv"), Paths.get("/Users/anja/Desktop/master/api/files/testing/stackoverflowNSR_new.csv"));
+        //addSecurityLabel("files/experiments/stackoverflowThreshold/stackoverflow_NSR.csv", "files/experiments/stackoverflowThreshold/stackoverflowNSR.csv", false);
+        //addSecurityLabel("files/experiments/stackoverflowThreshold/stackoverflow_SR.csv", "files/experiments/stackoverflowThreshold/stackoverflowSR.csv", true);
+
+        List<Path> paths = Arrays.asList(Paths.get("./files/experiments/tfidf/stackoverflow_CVE/stackoverflowSR.csv"), Paths.get("./files/experiments/stackoverflowNSR.csv"));
         List<String> mergedLines = getMergedLines(paths);
-        Path target = Paths.get("/Users/anja/Desktop/master/api/files/testing/stackoverflowDataset.csv");
+        Path target = Paths.get("files/experiments/tfidf/stackoverflow_CVE/stackoverflow_CVE.csv");
         Files.write(target, mergedLines, Charset.forName("UTF-8"));
     }
 
-    private static List<String> getMergedLines(List<Path> paths) throws IOException {
+    public static void merge(String file1, String file2, String newFile) throws IOException {
+        List<Path> paths = Arrays.asList(Paths.get(file1), Paths.get(file2));
+        List<String> mergedLines = getMergedLines(paths);
+        Path target = Paths.get(newFile);
+        Files.write(target, mergedLines, Charset.forName("UTF-8"));
+    }
+
+    public static List<String> getMergedLines(List<Path> paths) throws IOException {
         List<String> mergedLines = new ArrayList<>();
         for (Path p : paths){
             List<String> lines = Files.readAllLines(p, Charset.forName("UTF-8"));
@@ -28,5 +38,41 @@ public class MergeFiles {
             }
         }
         return mergedLines;
+    }
+
+    public static void addSecurityLabel(String filePath, String newFile, boolean security) throws IOException {
+        BufferedReader br = null;
+        BufferedWriter bw = null;
+
+        int sec = 0;
+        if(security){
+            sec = 1;
+        }
+
+        try {
+            br = new BufferedReader(new FileReader(filePath));
+            bw = new BufferedWriter(new FileWriter(newFile, false));
+
+            String line = "";
+            int i = 0;
+            while ((line = br.readLine()) != null) {
+                String[] cols = line.split(";");
+
+                if(i == 0){
+                    // add column for security report (1 = security, 0 != security)
+                    bw.write("Security;" + cols[0] + ";" + cols[1] + ";" + cols[2] + ";" + cols[3] + "\n");
+                } else {
+                    bw.write(sec + ";" + cols[0] + ";" + cols[1] + ";" + cols[2] + ";" + cols[3] + "\n");
+                }
+                i++;
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        } finally {
+            if (br != null)
+                br.close();
+            if (bw != null)
+                bw.close();
+        }
     }
 }
